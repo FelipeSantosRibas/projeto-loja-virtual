fcf8cf01f3d2969b888b7c860e5c755023ee77e1
Caio Oliveira <caio.oliveira@grupoviseu.com>
2026-09-13 01:20:34 -0300

    ajustes-aula

--- src/services/products.js
+++
@@ -1,8 +0,0 @@
-const API_URL = import.meta.env.VITE_API_URL
-
-export const getProducts = async () => {
-  const response = await fetch(`${API_URL}/products`);
-  const data = await response.json();
-
-  return data;
-};
